6532d96b2406fee5b1bcdb63a868bab63308a68e
Andrei <andreii.lapschow1999@mail.ru>
2020-12-07 19:48:46 +0400

    I have make test valid location in 7.2

--- laba7.sql
+++ laba7.sql
@@ -24,7 +24,7 @@ select ST_AsText(a.location) from address a
 where valid_location(a.location) is not null ;
 
 select ST_AsText(a.location) from address a
-where valid_location(a.location) is null ;
+where valid_location(a.location) is not null ;
 
 select ST_Y(ST_GeomFromText(valid_location(a.location)))as Y , ST_X(ST_GeomFromText(valid_location(a.location))) as X from address a
 where valid_location(a.location) is not null ;
@@ -39,14 +39,36 @@ drop procedure if exists area ;
 delimiter $$
 create procedure area(pol POLYGON)
 begin
-	select * , ST_AsText(a.location) from address a 
-    where ST_Contains(pol,a.location)=1;
+	select c.first_name, c.last_name , ST_AsText(a.location) as coordinates  from address a
+    inner join customer c on c.address_id=a.address_id
+    where ST_Contains(pol,a.location)=1 and valid_location(a.location)is not null;
 end$$
 delimiter ;
 
+call area( ST_PolygonFromText('POLYGON ((-10 -10,-10 10,10 10,10 -10,-10 -10))'));
 
 call area( ST_PolygonFromText('POLYGON ((-60 -40,-57.9 -37.3,-57.9 -34.3,-59.1 -34.3,-60 -40))')  );
 
-call area( ST_GeomFromText('POLYGON ((-60 -40,-57.9 -37.3,-57.9 -34.3,-59.1 -34.3,-60 -40))')  );
 
 
+#3. Для первого покупателя из указанной страны определить количество покупателей, 
+#   проживающих на заданном расстоянии (в градусах), используя функцию ST_Buffer.	
+ 	
+use sakila;
+
+drop procedure if exists neighbors ;
+
+delimiter $$
+create procedure neighbors(in arg_country int , in rad int)
+begin
+	select count(*)  from address a
+	where (st_contains(ST_Buffer((select a.location from customer cr
+	inner join address a on cr.address_id=a.address_id
+	inner join city on city.city_id=a.address_id
+	inner join country on country.country_id=city.country_id
+	where country.country_id=arg_country
+	limit 1) , rad) , a.location))=1;
+end$$
+delimiter ;
+
+call neighbors(79,100);
